6256c310bb5a7ff82ecd8581bc2ab7a3d100ffd2
FomichtheDev <fomichg@gmail.com>
2020-05-04 16:09:16 +0300

    JS + adaptive >= 768px

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.owl-carousel').owlCarousel({
         loop:true,
-        margin:30,
+        margin: 0,
         nav:true,
         autoplay: true,
         autoplayTimeout: 10000,
@@ -13,9 +13,39 @@ $(document).ready(function(){
                 items:1
             },
 
+            768:{
+                items:2
+            },
+
             1000:{
                 items:3
             }
         }
     });
+        $(".navbar-nav").on("click","a", function (event) {
+            event.preventDefault();
+            let id  = $(this).attr('href'), top = $(id).offset().top - 100;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+        $(".scrolltop").on("click", function (event) {
+            event.preventDefault();
+               let top = -$(this).offset().top;
+            $('body,html').animate({scrollTop: top}, 1500);
+        });
+        $(".switcher span.month").on("click" ,function () {
+            $(".switcher span").removeClass("active");
+            $(this).addClass("active");
+            $(".choose_table .item span.time").html("Monthly");
+            $(".choose_table .item:first-of-type span.price").html("$50");
+            $(".choose_table .item:nth-child(2) span.price").html("$150");
+            $(".choose_table .item:last-of-type span.price").html("$250");
+        });
+        $(".switcher span.year").on("click" ,function () {
+            $(".switcher span").removeClass("active");
+            $(this).addClass("active");
+            $(".choose_table .item span.time").html("Per year");
+            $(".choose_table .item:first-of-type span.price").html("$500");
+            $(".choose_table .item:nth-child(2) span.price").html("$1500");
+            $(".choose_table .item:last-of-type span.price").html("$2500");
+        })
 });
